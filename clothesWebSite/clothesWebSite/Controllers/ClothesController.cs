d030c23419b3e99dcccd2735d96c5172912cbe1f
Long Hoang <hoanglong13606@gmail.com>
2021-06-18 22:30:18 +0700

    first commit

---
+++ clothesWebSite/clothesWebSite/Controllers/ClothesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace clothesWebSite.Controllers
+{
+    public class ClothesController : Controller
+    {
+        // GET: Clothes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
