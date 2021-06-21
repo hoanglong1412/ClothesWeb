0af297950d8dbd3de60b98661bca8ddb6808e32d
Long Hoang <hoanglong13606@gmail.com>
2021-06-21 14:34:26 +0700

    get slider and product from database to view

--- clothesWebSite/Controllers/ClothesController.cs
+++ clothesWebSite/Controllers/ClothesController.cs
@@ -11,11 +11,16 @@ namespace clothesWebSite.Controllers
     public class ClothesController : Controller
     {
         ProductDAO productDAO = new ProductDAO();
+        BannerDAO bannerDAO = new BannerDAO();
         // GET: Clothes
         public ActionResult Index()
         {
             IEnumerable<Product> listPro = productDAO.getHotProduct(8);
+            IEnumerable<Banner> listBannerTop = bannerDAO.getBannerTop();
+            IEnumerable<Banner> listBannerBot = bannerDAO.getBannerBottom();
             ViewBag.product = listPro;
+            ViewBag.bannerT = listBannerTop;
+            ViewBag.bannerB = listBannerBot;
             return View();
         }
     }
---
+++ clothesWebSite/DAO/BannerDAO.cs
@@ -0,0 +1,25 @@
+﻿using clothesWebSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace clothesWebSite.DAO
+{
+    public class BannerDAO
+    {
+        MyDBContext db = new MyDBContext();
+        //Lay slider phia tren
+        public IEnumerable<Banner> getBannerTop()
+        {
+            IEnumerable<Banner> list = db.Banners.Where(m => m.state == 1);
+            return list;
+        }
+        //lay slider phia duoi muc inspired 
+        public IEnumerable<Banner> getBannerBottom()
+        {
+            IEnumerable<Banner> list = db.Banners.Where(m => m.state == 2);
+            return list;
+        }
+    }
+}
