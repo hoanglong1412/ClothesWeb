150817be3599eaaa6e18241d4a11637778022f90
Long Hoang <hoanglong13606@gmail.com>
2021-07-04 22:54:03 +0700

    order and order history

--- clothesWebSite/Controllers/CartController.cs
+++ clothesWebSite/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using clothesWebSite.Models;
 using clothesWebSite.DAO;
+using clothesWebSite.Models;
 
 namespace clothesWebSite.Controllers
 {
@@ -12,6 +13,9 @@ namespace clothesWebSite.Controllers
     {
         // GET: Cart
         ProductDAO productDAO = new ProductDAO();
+        PaymentDAO paymentDAO = new PaymentDAO();
+        PaymentDetailDAO paymentDetailDAO = new PaymentDetailDAO();
+        MyDBContext db = new MyDBContext();
         public ActionResult viewCart()
         {
             List<ShoppingCart> listCart = getCart();
@@ -22,6 +26,8 @@ namespace clothesWebSite.Controllers
             ViewBag.cart = listCart;
             return View(listCart);
         }
+
+        //lay gio hang
         public List<ShoppingCart> getCart()
         {
             List<ShoppingCart> listCart = Session["Cart"] as List<ShoppingCart>;
@@ -33,6 +39,7 @@ namespace clothesWebSite.Controllers
             return listCart;
         }
 
+        //them hang vao gio
         public ActionResult addToCart(int id, string strURL)
         {
             List<ShoppingCart> listCart = getCart();
@@ -50,6 +57,7 @@ namespace clothesWebSite.Controllers
             }
         }
 
+        //lay tong so luong hang trong gio
         private int countAll()
         {
             int count = 0;
@@ -61,6 +69,7 @@ namespace clothesWebSite.Controllers
             return count;
         }
 
+        //lay tong gia tien trong gio hang
         private double? totalAll()
         {
             double? total = 0;
@@ -72,6 +81,8 @@ namespace clothesWebSite.Controllers
             return total;
         }
 
+
+        //lay view dem so luong hang trong gio o layout shared
         public ActionResult CartPartialView()
         {
             ViewBag.AllCount = countAll();
@@ -79,6 +90,7 @@ namespace clothesWebSite.Controllers
             return PartialView();
         }
 
+        //xoa 1 san pham trong gio hang
         public ActionResult deleteRow(int id)
         {
             List<ShoppingCart> listCart = getCart();
@@ -92,11 +104,12 @@ namespace clothesWebSite.Controllers
 
 
         }
+
+        //cap nhat toan bo gio hang
         [HttpPost]
         public ActionResult updateCart(FormCollection form)
         {
             List<ShoppingCart> listCart = getCart();
-            //ShoppingCart product = listCart.Where(m => m.cart_product_id == id).FirstOrDefault();
             int i = 1;
             foreach (ShoppingCart p in listCart)
             {
@@ -107,6 +120,99 @@ namespace clothesWebSite.Controllers
             return RedirectToAction("viewCart");
 
 
+        }
+
+        //lay view cua tong so luong va tong so tien cua gio hang o trong view gio hang
+        public ActionResult orderSummaryPartialView()
+        {
+            ViewBag.AllCount = countAll();
+            ViewBag.AllTotal = totalAll();
+            return PartialView();
+        }
+
+        public ActionResult checkoutAddress()
+        {
+            if (Session["user"] == null || Session["user"].ToString() == "")
+            {
+                return RedirectToAction("Login", "User");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult checkoutDeliveryMethod(FormCollection form)
+        {
+            if (Session["user"] == null || Session["user"].ToString() == "")
+            {
+                return RedirectToAction("Login", "User");
+            }
+            ViewBag.name = form["name"];
+            ViewBag.address = form["address"];
+            ViewBag.phone = form["phone"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult checkoutPaymentMethod(FormCollection form)
+        {
+            if (Session["user"] == null || Session["user"].ToString() == "")
+            {
+                return RedirectToAction("Login", "User");
+            }
+            ViewBag.name = form["name"];
+            ViewBag.address = form["address"];
+            ViewBag.phone = form["phone"];
+            ViewBag.delivery = form["delivery"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult OrderPreview(FormCollection form)
+        {
+            if (Session["user"] == null || Session["user"].ToString() == "")
+            {
+                return RedirectToAction("Login", "User");
+            }
+            ViewBag.name = form["name"];
+            ViewBag.address = form["address"];
+            ViewBag.phone = form["phone"];
+            ViewBag.delivery = form["delivery"];
+            ViewBag.payment = form["payment"];
+            List<ShoppingCart> listCart = getCart();
+            ViewBag.AllCount = countAll();
+            ViewBag.AllTotal = totalAll();
+            ViewBag.cart = listCart;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Order(FormCollection form)
+        {
+            String name = form["name"];
+            String address = form["address"];
+            String phone = form["phone"];
+            String delivery = form["delivery"];
+            String paymentmethod = form["payment"];
+            User user = (User)Session["user"];
+            List<ShoppingCart> listCart = getCart();
+
+            Payment payment = new Payment();
+            payment.name_receiver = name;
+            payment.address_deliver = address;
+            payment.deliver_type = delivery;
+            payment.payway = paymentmethod;
+            payment.phone_receiver = phone;
+            payment.state = 1;
+            payment.user_id = user.user_id;
+            db.Payments.Add(payment);
+            db.SaveChanges();
+
+            foreach (var item in listCart)
+            {
+                paymentDetailDAO.addPaymentDetail(payment.payment_id, item.cart_product_id, item.cart_quantity, item.cart_sale_price);
+            }
+            Session["Cart"] = null;
+            return RedirectToAction("orderHistory","User");
         }
     }
 }
